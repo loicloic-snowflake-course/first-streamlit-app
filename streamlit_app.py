148bef7ec61de4b88df72fd1e6872d4d107774e7
loicloic-snowflake-course <134710534+loicloic-snowflake-course@users.noreply.github.com>
2023-06-02 15:16:03 +0200

    Update streamlit_app.py
    type my_ccur my_cur

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ if streamlit.button('View Our Fruit List - Add your favorites'):
 #streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_ccur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
     
